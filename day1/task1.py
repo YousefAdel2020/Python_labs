97552ca9d30a036be5f3f9e3f1409af8ee88a7b8
Yousef Adel <yousefjooo2018@gmail.com>
2023-04-27 04:46:38 +0200

    add lab1 part1

---
+++ day1/task1.py
@@ -0,0 +1,127 @@
+#task1
+
+# first_name=input('please enter your first name: ')
+# last_name=input('please enter your last name: ')
+# print(f"{last_name[::-1]} {first_name[::-1]}")
+
+
+#task2
+
+# num=int(input("please enter number"))
+# result=num+num**2+num**3
+# print(result)
+
+
+#task3
+
+# print("""a string that you "don't" have to escape
+# This
+# is a ....... multi-line
+# heredoc string --------> example
+# """)
+
+
+#task4
+
+# volume=(4/3)*(3.14)*(6**3)
+# print(volume)
+
+#task5
+
+# base=int(input('please enter base: '))
+# height=int(input('please enter height: '))
+# area=(0.5)*base*height
+# print(area)
+
+
+#task6
+# r=6
+
+# for row in range(r):
+#     for col in range(row):
+#         print("*",end=" ")
+#     print()
+
+# for row in range(r-1):
+#     for col in range(r-row-2):
+#         print("*",end=" ")
+#     print()
+
+#task7
+
+# word=input('please enter word: ')
+
+# print(f"{word[::-1]}")
+
+
+#task8
+
+# for num in range(7):
+#     if num==3 or num==6:
+#         continue
+#     print(num)
+
+
+#task9
+
+# def fib(n):
+#     a, b = 0, 1
+#     while a < n:
+#         print(a, end=' ')
+#         a, b = b, a+b
+#     print()
+
+# fib(50)
+
+#task10
+
+# text = input("Enter a string: ")
+
+# digits_counter = 0
+# letters_counter = 0
+
+# for char in text:
+#     if char.isdigit():
+#         digits_counter += 1
+#     elif char.isalpha():
+#         letters_counter += 1
+
+# print(digits_counter,letters_counter)
+
+##############################################################################
+
+#task1
+
+# def remove_adjacent_duplicates(numbers):
+#     result = []
+#     for number in numbers:
+#         if len(result) == 0 or number != result[-1]:
+#             result.append(number)
+#     return result
+
+# print(remove_adjacent_duplicates([1,1,1,2,3,3,3,4,4]))
+
+
+#task2
+
+# a=input("please enter a: ")
+# b=input("please enter b: ")
+# def split_string(x,y):
+
+#     x_front=x[:(len(x)//2)+len(x)%2]
+#     x_back=x[(len(x)//2)+len(x)%2:]
+
+#     y_front=x[:(len(y)//2)+len(y)%2]
+#     y_back=x[(len(y)//2)+len(y)%2:]
+
+#     return x_front + y_front + x_back + y_back
+
+# print(split_string(a,b))
+
+
+#task3
+
+def check_list(ls):
+    return len(ls)==len(set(ls))
+
+print(check_list([1,2,3,4]))
